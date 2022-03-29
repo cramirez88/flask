b0132c4bdcca07ecb8e385453e722ead7c04b83c
Christian Ramirez <christian.r0602@gmail.com>
2022-03-28 19:12:54 -0500

    first commit

--- fundamentals/create_server/server.py
+++ fundamentals/create_server/server.py
@@ -9,6 +9,19 @@ def hello():
 def dojo():
     return 'Dojo!'
 
+@app.route('/say/<name>')
+def print(name):
+    return f'Hi {name.capitalize()}'
+
+@app.route('/repeat/<int:num>/<string:name>')
+def repeat(num, name):
+    return f'{num * name}'
+
+@app.errorhandler(404)
+def pageNotFound():
+    return "Sorry! No response. Try again."
+
 
 if __name__== "__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode. after True add a comma and list another port number if some port number already is in usage
+    app.run(debug=True)  
+  # Run the app in debug mode. after True add a comma and list another port number if some port number already is in usage
